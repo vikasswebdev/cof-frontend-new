7ab97fef9863ee2eb42413c0aa90299c0de33676
VIKAS PATEL <63722887+vikas6377297826@users.noreply.github.com>
2023-07-20 17:35:50 +0530

    added analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/shared/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/shared/Footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="tag-manager-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P28SF2N');`,
+        }}
+      ></Script>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-ZLHPQRY7ZC`}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZLHPQRY7ZC', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
       <body className={inter.className}>
         <Navbar />
         <main className="min-h-screen">{children}</main>
